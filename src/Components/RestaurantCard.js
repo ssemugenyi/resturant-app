15555d4c7994549eb112341a8c894a8d0437a162
ssemugenyiisaac2@gmail.com <ssemugenyiisaac2@gmail.com>
2023-06-10 00:15:02 +0300

    (feat): Updated card details

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -17,11 +17,10 @@ const RestauranCard = () => {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          Dinner resturant
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          Delicious meals
         </Typography>
       </CardContent>
       <CardActions>
